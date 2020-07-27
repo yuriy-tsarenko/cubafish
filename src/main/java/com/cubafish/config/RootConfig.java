56272222aa9d06961e37f2049a1c106ff0235a22
Yuriy <tsarenkoyuriidev@gmail.com>
2020-07-27 19:48:35 +0300

    hotfix8:edit config (no xml config)

---
+++ src/main/java/com/cubafish/config/RootConfig.java
@@ -0,0 +1,9 @@
+package com.cubafish.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class RootConfig extends WebMvcConfigurerAdapter {
+
+}
